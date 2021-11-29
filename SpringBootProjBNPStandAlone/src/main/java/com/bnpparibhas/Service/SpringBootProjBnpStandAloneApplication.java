e32b8ff16fe8cabb5b9c615ced291ddacd090698
Dev2 <Dev2@gmail.com>
2021-11-29 12:47:07 +0530

    Intial import

---
+++ SpringBootProjBNPStandAlone/src/main/java/com/bnpparibhas/Service/SpringBootProjBnpStandAloneApplication.java
@@ -0,0 +1,13 @@
+package com.bnpparibhas.Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjBnpStandAloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjBnpStandAloneApplication.class, args);
+	}
+
+}
---
+++ SpringBootProjBNPStandAlone/src/main/java/com/bnpparibhas/Utility/Utility.java
@@ -0,0 +1,13 @@
+package com.bnpparibhas.Utility;
+
+import java.util.Date;
+
+public class Utility {
+	
+	
+	public Date getSysDate()
+	{
+		return new Date();
+	}
+
+}
