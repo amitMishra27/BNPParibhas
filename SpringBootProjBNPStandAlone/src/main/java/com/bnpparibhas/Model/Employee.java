31b1ccbe381943b2244c828cbd0b0bb78c82a855
Dev2 <Dev2@gmail.com>
2021-11-29 13:21:28 +0530

    Dev 2 Modified Emp java Utility java

--- SpringBootProjBNPStandAlone/src/main/java/com/bnpparibhas/Model/Employee.java
+++ SpringBootProjBNPStandAlone/src/main/java/com/bnpparibhas/Model/Employee.java
@@ -1,11 +1,55 @@
 package com.bnpparibhas.Model;
 
+import com.bnpparibhas.Utility.Utility;
+
 import lombok.Data;
 
 @Data
 public class Employee {
 	
 	private String empname;
+	
+	private String empjob;
+	
+	private Float empsal;
+	
+	private Integer empnum;
+	
+	public static void main(String[] args) {
+		System.out.println(Utility.getSysDate());
+	}
+
+	public String getEmpname() {
+		return empname;
+	}
+
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+
+	public String getEmpjob() {
+		return empjob;
+	}
+
+	public void setEmpjob(String empjob) {
+		this.empjob = empjob;
+	}
+
+	public Integer getEmpnum() {
+		return empnum;
+	}
+
+	public void setEmpnum(Integer empnum) {
+		this.empnum = empnum;
+	}
+
+	public Float getEmpsal() {
+		return empsal;
+	}
+
+	public void setEmpsal(Float empsal) {
+		this.empsal = empsal;
+	}
 
 	
 }
--- SpringBootProjBNPStandAlone/src/main/java/com/bnpparibhas/Utility/Utility.java
+++ SpringBootProjBNPStandAlone/src/main/java/com/bnpparibhas/Utility/Utility.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class Utility {
 	
 	
-	public Date getSysDate()
+	public static Date getSysDate()
 	{
 		return new Date();
 	}
